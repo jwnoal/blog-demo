589b1e859c30e7809720e10bf4ce50eabf18e226
jiangwei <jiangwei@tejin.com>
2023-02-09 10:51:53 +0800

    2

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ViewUIPlus from 'view-ui-plus'
 import 'view-ui-plus/dist/styles/viewuiplus.css'
-
+// t
 createApp(App).use(ViewUIPlus).mount('#app')
